fe95a745920feda7a8e130c17307351c90fed440
SRLABREEZE <srlabreeze@gmail.com>
2025-02-17 16:38:38 -0500

    globalStyles.js

---
+++ globalStyles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#121212',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#6200EE',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
